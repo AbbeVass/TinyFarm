df9a2296256b19ac298b559f572790ccdbfa9a79
Jacob Lewis Farnsworth <jacoblewis.farnsworth@ntig.se>
2023-11-20 09:42:22 +0100

    Fixed issue with tilemap paths

--- assets/tilemaps/spring and summer objects.tsx
+++ assets/tilemaps/spring and summer objects.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="spring and summer objects" tilewidth="16" tileheight="16" tilecount="169" columns="13">
- <image source="../assets/spring and summer objects.png" width="208" height="208"/>
+ <image source="../spring and summer objects.png" width="208" height="208"/>
 </tileset>
